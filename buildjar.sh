4e0e149d8746e704362a46c8969ef7fd7e426ea2
Dan Andre Paulsen <danandre@gmail.com>
2014-01-30 18:37:47 +0100

    Recalculate color codes to fix IDEA bug #IDEA-108424

--- buildjar.sh
+++ buildjar.sh
@@ -3,8 +3,10 @@
 mkdir colors
 cp Solarized\ Light.icls colors
 cp Solarized\ Dark.icls colors
+cp Solarized\ Darker.icls colors
 cp Solarized\ Light.icls colors/Solarized\ Light.xml
 cp Solarized\ Dark.icls colors/Solarized\ Dark.xml
+cp Solarized\ Darker.icls colors/Solarized\ Darker.xml
 touch IntelliJ\ IDEA\ Global\ Settings
 
 jar cfM settings.jar IntelliJ\ IDEA\ Global\ Settings colors
